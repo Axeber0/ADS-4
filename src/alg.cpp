f28b485466dd4892c0ce35494f041e78b41b0e3d
Axeber0 <axeber.yt@gmail.com>
2025-04-09 22:13:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,52 +1,54 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if ((arr[i] + arr[j]) == value) count++;
-        }
-    }
-    return count;
-}
-int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int finish = len - 1;
-    while (finish > 0 && arr[len] > value) finish--;
-    for (int i = 0; i < len; i++) {
-        for (int j = finish; j > i; j--) {
-            if ((arr[i] + arr[j]) == value) count++;
-        }
-    }
-    return count;
-}
-
-int countPairs3(int *arr, int len, int value) {
+int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    int key = value - arr[i];
-    int LeftSide = i + 1;
-    int RightSide = len - 1;
-    while (LeftSide <= RightSide) {
-      int Middle = LeftSide + (RightSide - LeftSide) / 2;
-      if (arr[Middle] == key) {
-        count++;
-        int chto_to = Middle - 1;
-        while (chto_to >= LeftSide && arr[chto_to] == key) {
-          count++; 
-          chto_to--;
-        }
-        chto_to = Middle + 1;
-        while (chto_to <= RightSide && arr[chto_to] == key) {
-          count++; 
-          chto_to++;
-        }
-        break;
-      } else if (arr[Middle] < key) {
-        LeftSide = Middle + 1;
-      } else {
-        RightSide = Middle - 1;
+      for (int j = i + 1; j < len; j++) {
+          if ((arr[i] + arr[j]) == value) count++;
       }
-    }
+  }
+  return count;
+}
+int countPairs2(int* arr, int len, int value) {
+  int count = 0;
+  int finish = len - 1;
+  while (finish > 0 && arr[finish] > value) finish--;
+  for (int i = 0; i < finish; i++) {
+      for (int j = finish; j > i; j--) {
+          if ((arr[i] + arr[j]) == value) count++;
+      }
+  }
+  return count;
+}
+
+int countPairs3(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+      int key = value - arr[i];
+      int left = i + 1;
+      int right = len - 1;
+      while (left <= right) {
+          int mid = left + (right - left) / 2;
+          if (arr[mid] == key) {
+              count++;
+              int temp = mid - 1;
+              while (temp >= left && arr[temp] == key) {
+                  count++;
+                  temp--;
+              }
+              temp = mid + 1;
+              while (temp <= right && arr[temp] == key) {
+                  count++;
+                  temp++;
+              }
+              break;
+          }
+          else if (arr[mid] < key) {
+              left = mid + 1;
+          }
+          else {
+              right = mid - 1;
+          }
+      }
   }
   return count;
 }
