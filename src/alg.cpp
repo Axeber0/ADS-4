d5c83a64871aba5d0a5b532379b1382bf9ec165d
Axeber0 <axeber.yt@gmail.com>
2025-04-09 23:22:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,22 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
+  const int* carr = arr;
   int count = 0;
   for (int i = 0; i < len; i++) {
       for (int j = i + 1; j < len; j++) {
-          if ((arr[i] + arr[j]) == value) count++;
+          if ((carr[i] + carr[j]) == value) count++;
       }
   }
   return count;
 }
 int countPairs2(int* arr, int len, int value) {
+  const int* carr = arr;
   int count = 0;
   int finish = len - 1;
-  while (finish > 0 && arr[finish] > value) finish--;
+  while (finish > 0 && carr[finish] > value) finish--;
   for (int i = 0; i < finish; i++) {
       for (int j = finish; j > i; j--) {
-          if ((arr[i] + arr[j]) == value) count++;
+          if ((carr[i] + carr[j]) == value) count++;
       }
   }
   return count;
@@ -22,26 +24,27 @@ int countPairs2(int* arr, int len, int value) {
 
 int countPairs3(int* arr, int len, int value) {
   int count = 0;
+  const int* carr = arr;
   for (int i = 0; i < len - 1; i++) {
-      int key = value - arr[i];
+      int key = value - carr[i];
       int left = i + 1;
       int right = len - 1;
       while (left <= right) {
           int mid = left + (right - left) / 2;
-          if (arr[mid] == key) {
+          if (carr[mid] == key) {
               count++;
               int temp = mid - 1;
-              while (temp >= left && arr[temp] == key) {
+              while (temp >= left && carr[temp] == key) {
                   count++;
                   temp--;
               }
               temp = mid + 1;
-              while (temp <= right && arr[temp] == key) {
+              while (temp <= right && carr[temp] == key) {
                   count++;
                   temp++;
               }
               break;
-          } else if (arr[mid] < key) {
+          } else if (carr[mid] < key) {
               left = mid + 1;
           } else {
               right = mid - 1;
