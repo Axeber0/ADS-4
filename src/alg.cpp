f4653e61d8447a05682a515690edcfeee299a112
Axeber0 <axeber.yt@gmail.com>
2025-04-09 19:19:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,49 +1,42 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-      for (int j = i; j < len; j++) {
-          if (i == j) continue;
-          if ((arr[i] + arr[j]) == value) count++;
-      }
-  }
-  return count;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (i == j) continue;
+            if ((arr[i] + arr[j]) == value) count++;
+        }
+    }
+    return count;
 }
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  while (arr[len] > value) len--;
-  for (int i = 0; i < len; i++) {
-      for (int j = len; j > i; j--) {
-          if ((arr[i] + arr[j]) == value) count++;
-      }
-  }   
-  return count;
+    int count = 0;
+    while (arr[len] > value) len--;
+    for (int i = 0; i < len; i++) {
+        for (int j = len; j > i; j--) {
+            if ((arr[i] + arr[j]) == value) count++;
+        }
+    }
+    return count;
 }
 
-int binar(int* arr, int len, int value) {
-  int left = 0;
-  int right = len;
-
-  while (left <= right) {
-      int mid = left + (right - left) / 2; 
-
-      if (arr[mid] == value) {
-          return mid; 
-      }
-      else if (arr[mid] < value) {
-          left = mid + 1; 
-      }
-      else {
-          right = mid - 1; 
-      }
-  }
-
-  return -1;
-}
 int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-      if (arr[i] + arr[binar(arr, len, (value - arr[i]))] == value) count++;
-  }
-  return count/2;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int key = value - arr[i];
+        int LeftSide = i + 1;
+        int RightSide = len - 1;
+        while (LeftSide <= RightSide) {
+            int Middle = LeftSide + (RightSide - LeftSide) / 2;
+            if (arr[Middle] == key) {
+                count++;
+                while (Middle - 1 >= LeftSide && arr[Middle - 1] == key) { count++; Middle -= 2; }
+                while (Middle + 1 <= RightSide && arr[Middle + 1] == key) { count++; Middle += 2; }
+                break;
+            }
+            else if (arr[Middle] < key) LeftSide = Middle + 1;
+            else RightSide = Middle - 1;
+        }
+    }
+    return count;
 }
