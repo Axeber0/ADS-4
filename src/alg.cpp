be48d10cce51e0cdcda96d179baa453e965317c5
Axeber0 <axeber.yt@gmail.com>
2025-04-09 22:16:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(const int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
       for (int j = i + 1; j < len; j++) {
@@ -8,7 +8,7 @@ int countPairs1(int* arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(const int* arr, int len, int value) {
   int count = 0;
   int finish = len - 1;
   while (finish > 0 && arr[finish] > value) finish--;
@@ -20,7 +20,7 @@ int countPairs2(int* arr, int len, int value) {
   return count;
 }
 
-int countPairs3(int* arr, int len, int value) {
+int countPairs3(const int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
       int key = value - arr[i];
